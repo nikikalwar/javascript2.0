3ab9067d8aed7e1c9fe00116eaebf9cdfe6d75a8
Niki Kalwar <nikikumar27@gmail.com>
2022-01-19 14:18:38 +0530

    interchanging values

---
+++ 1.js
@@ -0,0 +1,11 @@
+var a = 3;
+var b = 8;
+
+//interchanging the values
+var c;
+c=a;
+a=b;
+b=c;
+
+console.log("the value of a is "+a);
+console.log("the value of b is "+b);
---
+++ index.js
@@ -0,0 +1,16 @@
+/*
+//alert("hi");
+var myName=" Jane";
+myName="Something else";
+
+console.log(myName);
+alert(myName);
+let new_name=prompt("Enter new name");
+console.log("the new name is "+new_name);
+*/
+
+var gameLevel=1;
+for(let i=0;i<100;i++){
+    console.log("gameLevel"+i);
+}
+
